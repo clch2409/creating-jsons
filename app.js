c6ce9817763e9aa6b7a9fd920faf8e487c977a93
clch2409 <cesarlch2409@gmail.com>
2025-03-06 16:17:02 -0500

    Creando el formulario para los insumos por medio de obtener datos de la consola

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('priemr proyecto');
+import readline from 'node:readline/promises';
+
+let insumos = []
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.on('close', () => {
+  process.exit(0);
+})
+
+while(true){
+  const nombre = await rl.question('Escribe el nombre del insumo: ')
+  const precio = await rl.question('Escribe el precio del insumo: ')
+  const proveedor = await rl.question('Escribe el proveedor del insumo: ')
+  const tipoDeInsumo = await rl.question('Escribe el tipo de insumo: ')
+  const continuar = await rl.question('Deseas agregar otro insumo? (S/N): ')
+
+  if(continuar === 'N') break
+
+  insumos.push({
+    nombre,
+    precio,
+    proveedor,
+    tipoDeInsumo
+  })
+
+  rl.close()
+}
+
+console.log(insumos)
